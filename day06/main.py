57f04dde34d03df47e4d0ddafd0bba0463d17e59
Alan Smith <alan@randomsmiths.com>
2023-12-06 10:51:08 -0500

    Part 2

--- day06/main.py
+++ day06/main.py
@@ -6,9 +6,13 @@ def parseArgs():
   parser.add_argument('infile', help='input file')
   return parser.parse_args()
 
-def parse(line):
+def parse_part1(line):
   (before, after) = line.strip().split(":")
   return [int(time) for time in after.split(' ') if time]
+
+def parse_part2(line):
+  (before, after) = line.strip().split(":")
+  return int("".join([time for time in after.split(' ') if time]))
 
 def numberWays(milliseconds, millimeters):
    num_ways = 0
@@ -16,9 +20,15 @@ def numberWays(milliseconds, millimeters):
       speed = x
       time = milliseconds - x
       if (time * speed) > millimeters:
-         num_ways += 1
+         num_ways += 1        
+      if(x % 100_000 == 0):
+         print(f"X: {x}")    
    return num_ways
-      
+
+def part2(time, distance):
+   print(f"{time} {distance}")
+   print(f"{numberWays(time, distance)}")
+
 def part1(times, distances):
   total = 1
   for i in range(len(times)):
@@ -30,9 +40,15 @@ if __name__ == "__main__":
   args = parseArgs()
   times = []
   distances = []
+  time = 0
+  distance = 0
+
   for line in open(args.infile, 'r'):
      if len(times) == 0:
-        times = parse(line)
+        times = parse_part1(line)
+        time = parse_part2(line)
      else:
-        distances = parse(line)
+        distances = parse_part1(line)
+        distance = parse_part2(line)
   part1(times, distances)
+  part2(time, distance)
