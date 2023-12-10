905c69f011d96e15879b0bbee18cae6c98fe4a81
Alan Smith <alan@randomsmiths.com>
2023-12-10 15:20:13 -0500

    Day 8

--- day08/main.py
+++ day08/main.py
@@ -1,5 +1,6 @@
 import argparse
 import re
+from math import lcm
 
 def parseArgs():
   parser = argparse.ArgumentParser()
@@ -30,6 +31,45 @@ def part1(directions, dictMaps):
      steps += 1
   print(f"Num Steps: {steps}")
 
+def done(locations):
+    for location in locations:
+      if location[2] != 'Z':
+          return False
+    return True
+
+def part2(directions, dictMaps):
+  steps = 0
+  locations = []
+  stepsZ = []
+  for location in dictMaps.keys():
+    if location[2] == 'A':
+        locations.append(location)
+        stepsZ.append(0)
+
+  while not done(locations):     
+    nextDir = directions.get_next()
+    if nextDir == 'L':        
+        for i, location in enumerate(locations):
+          locations[i] = dictMaps[location][0]
+    else:
+        for i, location in enumerate(locations):
+          locations[i] = dictMaps[location][1]
+    steps += 1
+    for i, location in enumerate(locations):
+       if location[2] == 'Z':
+          if stepsZ[i] == 0:
+             stepsZ[i] = steps
+    
+    totalMultiplied = 1
+    for z in stepsZ:
+       totalMultiplied *= z
+
+    if totalMultiplied:
+       print(f"Total by Multiplication: {totalMultiplied} {stepsZ}")   
+       print(f"LCM: {lcm(*stepsZ)}")       
+
+  print(f"Part 2 Num Steps: {steps}")
+
 if __name__ == "__main__":
   args = parseArgs()
   dictMaps = {}
@@ -41,6 +81,5 @@ if __name__ == "__main__":
             dictMaps[m.group(1)] = (m.group(2), m.group(3))
      else:        
         directions = Directions(line)
-  print(dictMaps)
-  print(directions)
-  part1(directions, dictMaps)
+  # part1(directions, dictMaps)
+  part2(directions, dictMaps)
